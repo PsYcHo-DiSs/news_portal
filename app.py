5cea0d851132028e8b629e94308a0db27aa84a3f
Artem Lobzov <psychodiss@gmail.com>
2025-03-09 14:16:36 +0300

    initialized project, added nav bar, added db connection

---
+++ app.py
@@ -0,0 +1,55 @@
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+# create the extension
+db = SQLAlchemy()
+
+# create the app
+app = Flask(__name__)
+
+# configure the Postgres database
+app.config['SQLALCHEMY_DATABASE_URI'] = f"postgresql://postgres:123456@127.0.0.1/news"
+
+# initialize the app with the extension
+db.init_app(app)
+
+# Create Model
+from datetime import datetime
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import mapped_column
+
+
+class Category(db.Model):
+    """Категории постов"""
+    __tablename__ = 'category'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(150))
+    posts = db.relationship('Post', back_populates='category')
+
+    def __repr__(self):
+        return self.title
+
+
+class Post(db.Model):
+    """Новостные посты"""
+    __tablename__ = 'post'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(150))
+    content = db.Column(db.Text)
+    created = db.Column(db.DateTime, default=datetime.now())
+    category_id = mapped_column(ForeignKey('category.id'))
+    category = db.relationship('Category', back_populates='posts')
+
+    def __repr__(self):
+        return self.title
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    """Главная страница"""
+    return render_template('news/index.html', title='Главная')
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='192.168.0.160', port=8000)
