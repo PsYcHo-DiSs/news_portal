1ffe72ded4e7d5177d772b8231a6c320da73146a
Artem Lobzov <psychodiss@gmail.com>
2025-03-19 19:45:05 +0300

    installed flask-login - Added flask-login package to handle user authentication - installed python-dotenv - refactoring for outside keeping confidential project info

--- news/__init__.py
+++ news/__init__.py
@@ -1,16 +1,26 @@
-from flask import Flask
+from flask import Flask, session
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+
+from dotenv import load_dotenv
+import os
+
+# Загружаем переменные окружения
+load_dotenv()
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = f"postgresql://postgres:123456@127.0.0.1/news"
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
+
 # app.config['SQLALCHEMY_ECHO'] = True
 
-UPLOAD_FOLDER = 'news/static/images'
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['UPLOAD_FOLDER'] = os.getenv('UPLOAD_FOLDER')
 
-app.config['SECRET_KEY'] = '555_777'
-app.config['WTF_CSRF_ENABLED'] = True
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+app.config['WTF_CSRF_ENABLED'] = os.getenv('WTF_CSRF_ENABLED') == "True"
+
+# login_manager = LoginManager()
+# login_manager.init_app(app)
 
 db = SQLAlchemy()
 db.init_app(app)
