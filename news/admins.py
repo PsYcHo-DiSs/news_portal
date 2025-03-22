0b036ba0b0eac39f81f5c54ee64eb59687ddc84b
Artem Lobzov <psychodiss@gmail.com>
2025-03-22 16:57:29 +0300

    - plus admin panel only for .is_staff users,

--- news/admins.py
+++ news/admins.py
@@ -2,11 +2,11 @@ import os
 import uuid as uuid
 from werkzeug.utils import secure_filename
 
-from flask_admin import Admin
+from flask_admin import Admin, AdminIndexView
+from flask_login import current_user
+from flask import redirect, url_for, flash
 from flask_admin.contrib.sqla import ModelView
 from flask_admin.form import FileUploadField
-
-from flask_login import current_user
 
 from news import app, db
 from news.models import Category, Post, Users
@@ -15,6 +15,16 @@ from news.models import Category, Post, Users
 def prefix_name(obj, file_data):
     parts = os.path.splitext(file_data.filename)
     return secure_filename(f'{uuid.uuid4()}_%s%s' % parts)
+
+
+class AdminAccess(AdminIndexView):
+    def is_accessible(self):
+        if not current_user.is_anonymous:
+            return current_user.is_staff
+
+    def inaccessible_callback(self, name, **kwargs):
+        flash("У вас нет полномочий для входа в админ панель!", "error")
+        return redirect(url_for('index'))
 
 
 class PostAdmin(ModelView):
@@ -38,7 +48,7 @@ class UsersAdmin(ModelView):
     }
 
 
-admin = Admin(app, template_mode='bootstrap4')
+admin = Admin(app, template_mode='bootstrap4', index_view=AdminAccess())
 admin.add_view(ModelView(Category, db.session))
 admin.add_view(PostAdmin(Post, db.session))
 admin.add_view(UsersAdmin(Users, db.session))
